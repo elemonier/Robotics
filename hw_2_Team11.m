0ac15417b02ce0cb26f4931f7c4f05e25622b51f
henriquetmaia <htm2104@columbia.com>
2013-10-05 00:53:05 -0400

    Working Hw 2 Code, No Extra Credit

--- hw_2_Team11.m
+++ hw_2_Team11.m
@@ -9,217 +9,224 @@
 % Team 11 - The Pumpkin Widgets
 
 function hw_2_Team11(serPort)
-
-    global Goal td Angle ClosestX ClosestY;
-    td = .05;   % overwrite/increase default td so functions crash less
     
-    SetDriveWheelsCreate(serPort, 0, 0) % Stop collaborate and listen.
-    DistanceSensorRoomba(serPort);  %reset the distance sensor
-    AngleSensorRoomba(serPort);     %reset the angle sensor
+    %=============================================================%
+    % Description                                                 %
+    %=============================================================%
+    % This is a simple solution for HW 2 built on HW 1's solution %
+    %=============================================================%
     
-    Goal = 0;
-    Angle = 0;                      %always start facing goal
-    ClosestX = inf;
-    ClosestY = inf;
+    %%
+    %=============================================================%
+    % Clear cache && avoid NaN values                             %
+    %=============================================================%
+    clc;                                                          % Clear the cache
+                                                                  % #ClicLab4Ever
+    % Poll for bump Sensors to avoid getting NaN values when the 
+    % robot first hits a wall
+    [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster ...
+              BumpFront] = BumpsWheelDropsSensorsRoomba(serPort); % Avoid NaaN, yum
+          
+    WallSensorReadRoomba(serPort);       % Reset Everything at start?
+    DistanceSensorRoomba(serPort);      
+    AngleSensorRoomba(serPort);           
+    %=============================================================%
+    
+    %%
+    %=======================%
+    % Position Declaration  %
+    %=======================%
+    
+    % Current Position
+    current_pos_normal = 0;     % Normal to M-Line path
+    current_pos_tangential = 0; % Along the M-Line direction
+    current_angle = 0;
+    prev_proximity = inf;       % To ensure we are approaching
+    
+    % Hit Position Variables
+    hit_pos_normal = 0;
+    hit_pos_tangential = 0;
+    hit_angle = 0;
+    hit_proximity_to_goal = inf;
+    
+    %%
+    %=======================%
+    % Velocity Declaration  %
+    %=======================%
+    velocity_val = 0.2;
+    angular_velocity_val = 0.1;
+    %=======================%
 
-    while ~Goal % will be set to true by Goal Reached in Odom    
+    %=======================%
+    % Distance Thresholds   %
+    %=======================%
+    dist_thresh_from_goal = 0.3;
+    dist_thresh_from_hit  = 0.2;
+    slope_thresh_for_m_line = 0.1; %%% experiment with thizz bound
+    goal_distance = 10.0;          % in the tangential direction (Initial Forward)
+    %=======================%
+   
+    %=======================%
+    % Status Variable       %
+    %=======================%
+    status = 1;             % 1 -> Move Forward, 
+                            % 2 -> Wall Follow | Haven't left the threshold of the hit point
+                            % 3 -> Wall Follow | Left the threshold of the hit point
+                            % 4 -> On M-Line so Turn towards Goal  
+                            % 5 -> Stop at Goal Position, Success
+                            % 6 -> Trapped and thus Failure
+                            % 5 -> Turn around if moving away on M-Line
+    %=============================================================%
+    
+    %% Main Loop
+    while 1 
         
-        turnToGoal(serPort);
+        %%
+        %=============================================================%
+        % Step 1 - Read Sensors                                       %
+        %=============================================================%
+        % For each loop, first read data from the robot sensor
+        [BumpRight, BumpLeft, ~, ~, ~, BumpFront]  ...
+            = BumpsWheelDropsSensorsRoomba(serPort);                  % Poll for Bump Sensors
+        Wall = WallSensorReadRoomba(serPort);                         % Poll for Wall Sensor
+        distance_temp = DistanceSensorRoomba(serPort);                % Poll for Distance delta
+        angle_temp = AngleSensorRoomba(serPort)*180/pi;                      % Poll for Angle delta
+        %=============================================================%
+        
+        %%
+        %=============================================================%
+        % Step 2 - Update Odometry                                    %
+        %=============================================================%
+        % Keep tracking the position and angle in space
+        current_angle = mod((current_angle + angle_temp + 360), 360);
+        current_pos_normal = current_pos_normal + sin(current_angle*pi/180) * distance_temp;
+        current_pos_tangential = current_pos_tangential + cos(current_angle*pi/180) * distance_temp;
+        
+        hit_angle = hit_angle + angle_temp;
+        hit_pos_normal = hit_pos_normal + sin(hit_angle*pi/180) * distance_temp;
+        hit_pos_tangential = hit_pos_tangential + cos(hit_angle*pi/180) * distance_temp;
+        
+        drawnow;            % not entirely sure what thizzz is ?
+        %=============================================================%
+        
+        %%
+        %=============================================================%
+        % Step 3 - Calculate Euclidean Distances                      %
+        %=============================================================%
+        hit_distance = sqrt(hit_pos_normal ^ 2 + hit_pos_tangential ^ 2); % point of reference
+        proximity = sqrt( (goal_distance - current_pos_tangential) ^2 + current_pos_normal^ 2); % are we there yet?
+        slope_m = abs(current_pos_normal / current_pos_tangential); % used to fix orientation
+        %=============================================================%
 
-        moveToGoal(serPort);
-        
-        wallFollow(serPort);
-        
+
+        %%
+        %=============================================================%
+        % Step 3.5 - Debugging                                        %
+        %=============================================================%
+        display(status)
+        display(current_pos_normal)
+        display(current_pos_tangential)
+        display(proximity);
+        display(hit_proximity_to_goal);
+        display(angle_temp);
+        display(current_angle)
+        display(slope_m);
+
+        %%
+        %=============================================================%
+        % Step 4 - Check Status                                       %
+        %=============================================================%
+        switch status
+            case 1 % Move Forward, Theoretically should always be on M-Line
+                if (proximity < dist_thresh_from_goal)
+                    status = 5;
+                elseif (slope_m > slope_thresh_for_m_line ) %fix orientation
+                    status = 4;
+                elseif (prev_proximity < proximity) % turn around, moving away from goal
+                    status = 7;
+                elseif (BumpRight || BumpLeft || BumpFront)
+                    status = 2; % Change Status to Wall Follow
+                    hit_angle = 0;  % Mark this as origin of Hit Coord Space
+                    hit_pos_normal = 0;  % So we know if we return here
+                    hit_pos_tangential = 0;  % and mark proximity in case we
+                    hit_proximity_to_goal = proximity; % re-encounter the M-Line
+                end
+                prev_proximity = proximity;
+                SetFwdVelAngVelCreate(serPort, velocity_val, 0 );
+            case 2 % Wall Follow | Haven't left the threshold of the hit point
+                WallFollow(velocity_val, angular_velocity_val, BumpRight, BumpLeft, BumpFront, Wall, serPort);
+                if (hit_distance > dist_thresh_from_hit)
+                    status = 3;
+                end
+            case 3 % Wall Follow | Left the threshold of the hit point
+                WallFollow(velocity_val, angular_velocity_val, BumpRight, BumpLeft, BumpFront, Wall, serPort);
+                %check for M-line and closer than hit point   
+                if (slope_m < slope_thresh_for_m_line && proximity < hit_proximity_to_goal)
+                   status = 4;
+                elseif(hit_distance < dist_thresh_from_hit)
+                   status = 6; % womp
+                end
+            case 4 % Orient yourself on M-Line and just keep swimming            
+                turnAngle(serPort, angular_velocity_val, -current_angle);
+                if (330 < current_angle) || (current_angle < 30) % experiment with thizzz
+                    SetFwdVelAngVelCreate(serPort, velocity_val, 0 );
+                    status = 1;
+                end
+            case 5 % SUCCESS, Victory Spin
+                fprintf('Robot stopped at Goal point\n');
+                SetFwdVelAngVelCreate(serPort, 0, 0 );
+                fprintf('Robot is ready for next command\n');
+                return;
+            case 6 % FAILURE
+                fprintf('Robot failed to reach goal\n');
+                SetFwdVelAngVelCreate(serPort, 0, 0 );
+                fprintf('[ERROR] :: DELETING FOLLOWING LAWS\n');
+                fprintf('A robot may not injure a human being\n');
+                fprintf('or, through inaction, allow a human being\n');
+                fprintf('to come to harm.\n\n');
+                fprintf('A robot must obey the orders given to it by\n');
+                fprintf('human beings, except where such orders would\n');
+                fprintf('conflict with the First Law.\n\n');
+                fprintf('A robot must protect its own existence\n');
+                fprintf('as long as such protection does not\n');
+                fprintf('conflict with the First or Second Law.\n');
+                return;
+            case 7 % passed the goal
+                SetFwdVelAngVelCreate(serPort, 0, 0 );
+                turnAngle(serPort, angular_velocity_val, 180); % not sure if complete solution, but has worked
+                status = 1;
+        end
     end
-
-    SetDriveWheelsCreate(serPort, 0, 0);  % Full Stop
-    display('FINISHED');                  % Home Sweet Home
 end
 
-function turnToGoal(serPort)
-    global Angle Exit;
-    Exit = 0;
+%%
+% Wall Follow Function
+function WallFollow(velocity_val, angular_velocity_val, BumpRight, BumpLeft, BumpFront, Wall, serPort)
+
+    % Angle Velocity for different bumps
+    av_bumpright =  4 * angular_velocity_val;
+    av_bumpleft  =  2 * angular_velocity_val;
+    av_bumpfront =  3 * angular_velocity_val;
+    av_nowall    = -4 * angular_velocity_val;
     
-    while Angle ~= 0
-        %turn SLOWLY until global angle is zero
-        pause(0.1);
-    end
-
-end
-
-function moveToGoal(serPort)
-    global Goal;
-    
-    %Hit = check_if_Hit
-    while ~Goal && ~Hit && ~Exit
-        %move forward
-        pause(0.1);
-        %Hit = check_if_Hit
-        %%%%%%%stop? -- not needed since taken care of in Odom?
-        %get info on current position
-        %pass info to Odom (check Goal) (will call stop if on M-Line)
-        pause(0.1);
-    end
-end
-
-function Odom
-    global Goal Angle ClosestX ClosestY DistanceToGoal Exit;
-    
-    %calculate if on M-Line
-    
-    if on the M-Line 
-        SetDriveWheelsCreate(serPort, 0, 0) % Stop
-        %DistanceToGoal = calculate distance to goal
-        
-        if DistanceToGoal == 0
-            display('SUCCESS');
-            Goal = 1;
-        elseif %closer than last hit coordinate
-            Exit = 1;
-        elseif %BACK at closest hit point (make sure doesnt trigger at the original hit at this point)
-            display('FAILURE');
-            Goal = 1; % Go home, you're drunk
-        end
-    end
-        
-
-end
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Old Code from hw 1 below
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-
-function wallFollow(serPort)
-    global Goal Exit;
-
-    avg_count = 3; %how many polls of the sensor to make
-    d = 0;
-    searchingForWall = 0;
-    MIN_WALL_STRENGTH = 15; % minimal wall strength 
-    
-    while ~Goal && ~Exit
-
-        [BumpRight, ~, ~, ~, ~, BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
-        wallStrength = 0;
-        if(~isnan(BumpRight) || ~isnan(BumpFront))
-            if (BumpFront)
-                display('Bump Front')
-                SetDriveWheelsCreate(serPort, 0, 0);    
-                turnAngle(serPort, 0.1, 30); %turn away from front bump and locate wall
-                display('Survey')                
-                Survey(serPort)
-                %update odometry
-                UpdateOdometry(AngleSensorRoomba(serPort), DistanceSensorRoomba(serPort));
-
-                wallStrength = AnalogWallSensorReadRoomba(serPort);
-            end
-            if (BumpRight)
-%                 display(BumpRight)
-%                 SetDriveWheelsCreate(serPort, 0, 0);
-                SetDriveWheelsCreate(serPort, -0.1, 0.1);
-                pause(0.1);
-                Survey(serPort)
-            end
-            
-        end
-        max = 0;
-        i = 0;
-        while i < avg_count
-            SetDriveWheelsCreate(serPort, 0, 0);
-            pause(0.1)
-            wallStrength = AnalogWallSensorReadRoomba(serPort); 
-            pause(0.1)
-            if(isnan(wallStrength))
-                i = i - 1;
-                continue;
-            end
-            if max < wallStrength
-                max = wallStrength;
-            end
-            i = i + 1;
-        end
-        wallStrength = max;
-        
-        display(wallStrength)
-        SetDriveWheelsCreate(serPort, 0, 0);
-        pause(0.05)
-       
-        if(wallStrength > MIN_WALL_STRENGTH)
-            SetDriveWheelsCreate(serPort, 0.15, 0.15); 
-            pause(0.2)
-            display('onward');
-        else
-            SetFwdVelRadiusRoomba(serPort, 0.1, -0.1);
-            display('lost wall sensor');
-            UpdateOdometry(AngleSensorRoomba(serPort), DistanceSensorRoomba(serPort));
-
-        end
-        
-        pause(0.1);
-    end
-
-end
-
-%Survey in a circle for the strongest wall-IR-sensor reading
-%Should align itself orthogonal to wall once completed
-%If it cannot find a wall it spirals in search of one
-function Survey(serPort)
-    prevStrength = 0;
-    w_speed = 0.05;
-    turn_count = 0;
-    while turn_count < 30
-        SetDriveWheelsCreate(serPort, w_speed, -w_speed);   % turn away from wall
-        pause(0.1);
-        SetDriveWheelsCreate(serPort, 0, 0);                % Stop for steady signal
-        pause(0.1);
-        wallStrength = AnalogWallSensorReadRoomba(serPort); % Scan for signal
-        pause(0.1)
-        if(prevStrength >= wallStrength && prevStrength > 10) % if signal was valid and decreases
-            break;                                            %stop scanning
-        end
-        if (wallStrength > prevStrength + 4) % if slight improvement
-            prevStrength = wallStrength;     % accept new value
-        end
-        turn_count = turn_count + 1;
-        pause(0.1)
-    end
-    if(turn_count == 30)                            % if you went full circle and never found a signal
-        SetFwdVelRadiusRoomba(serPort, 0.1, -0.15); % spin around to find the wall
-        display('find the wall');        
+    if BumpRight || BumpLeft || BumpFront
+        v = 0;                              % Set Velocity to 0
+    elseif ~Wall
+        v = 0.25 * velocity_val;            % Set Velocity to 1/4 of the default
     else
-        SetDriveWheelsCreate(serPort, -w_speed, w_speed); % pivot back once to return to parallel
-        pause(0.2)
-        SetDriveWheelsCreate(serPort, 0, 0);
-    end
-end
-
-
-function UpdateOdometry(angle, distance)
-    global TotalX TotalY TotalAngle NetDistance;
-    
-    angle = angle * 180 / pi;
-    disp('angle before');
-    disp(angle);
-    disp('distance');
-    disp(distance);
-    
-    if(angle < -180)
-        angle = angle + 360;
+        v = velocity_val;                   % Set Velocity to the default
     end
 
-    TotalAngle = TotalAngle + angle;   
-
-    TotalX = TotalX + (cos(TotalAngle * pi/180)*distance)*100;
-    TotalY = TotalY + (sin(TotalAngle * pi/180)*distance)*100; 
-    
-    fprintf('x: %8.4f\ny: %8.4f\na: %6.3f\n', TotalX, TotalY, TotalAngle);
-    absAng = abs(abs(TotalAngle) - 360);
-    if (absAng < 15)
-        if(abs(TotalX) < 80 && abs(TotalY) < 80)
-            NetDistance = 0;
-        end
+    if BumpRight
+    av = av_bumpright;                      % Set Angular Velocity to av_bumpright
+    elseif BumpLeft
+        av = av_bumpleft;                   % Set Angular Velocity to av_bumpleft
+    elseif BumpFront
+        av = av_bumpfront;                  % Set Angular Velocity to av_bumpfront
+    elseif ~Wall
+        av = av_nowall;                     % Set Angular Velocity to av_nowall
+    else
+        av = 0;                             % Set Angular Velocity to 0
     end
-     
+    SetFwdVelAngVelCreate(serPort, v, av );
 end
